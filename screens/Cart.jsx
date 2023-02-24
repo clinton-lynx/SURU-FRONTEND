0625638f271380b9885fe475f760d0bc3cd62625
Clinton Adeoti <clintonadeoti1@gmail.com>
2023-02-24 11:55:14 -0800

    fix: folder restructure

--- screens/Cart.jsx
+++ screens/Cart.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { View, Text, ScrollView, StyleSheet, Image } from "react-native";
-import { Arrow } from "../assets/icons/Icons";
+import { Arrow, Car, Line, Star } from "../assets/icons/Icons";
 
 const Cart = () => {
   return (
     <ScrollView
-    style={{
-      backgroundColor: "#Fff",
-      flex: 1,
-      paddingTop: 40,
-      paddingBottom: 50,
-      paddingHorizontal: 16,
-    }}
-  >
+      style={{
+        backgroundColor: "#Fff",
+        flex: 1,
+        paddingTop: 40,
+        paddingBottom: 50,
+        paddingHorizontal: 20,
+      }}
+    >
       <View style={styles.arrowBackWrapper}>
         <Arrow />
       </View>
@@ -23,14 +23,37 @@ const Cart = () => {
           source={require("../assets/images/yam.png")}
         />
       </View>
-      <View>
-        <Text>banana</Text>
+      <View style={styles.itemNameWrapper}>
+        <Text style={styles.itemName}>banana</Text>
       </View>
-      <View>
-        <Text>N600</Text>
+      <View style={styles.itemPriceWrapper}>
+        <Text style={styles.itemPrice}>N600</Text>
       </View>
-      <View>
-       <Text>delivery and ratings</Text>
+      <View style={styles.itemRatingsWrapper}>
+        <View style={styles.itemRatingsLeft}>
+          <View style={styles.starIcon}>
+            <Star />
+          </View>
+          <View style={styles.itemRatingsRatioWrapper}>
+            <Text style={styles.itemRatingsRatio}>4.5</Text>
+          </View>
+          <View style={styles.itemRatingsTotalWrapper}>
+            <Text style={styles.itemRatingsTotal}>7.0 ratings</Text>
+          </View>
+        </View>
+        <View style={styles.itemRatingsMiddle}>
+          <View style={styles.dividerIcon}>
+            <Line />
+          </View>
+        </View>
+        <View style={styles.itemRatingsRight}>
+          <View style={styles.carIcon}>
+            <Car />
+          </View>
+          <View style={styles.itemDeliveryWrapper}>
+            <Text style={styles.itemDelivery}>Free delivery</Text>
+          </View>
+        </View>
       </View>
     </ScrollView>
   );
@@ -38,4 +61,37 @@ const Cart = () => {
 
 export default Cart;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imageWrapper: {
+    backgroundColor: "#FFD600",
+    paddingHorizontal: 26,
+    paddingVertical: 8,
+    marginTop: 10,
+    marginBottom: 20,
+    borderRadius: 20,
+  },
+  image: {
+    alignSelf: "center",
+  },
+  itemNameWrapper: {},
+  itemName: {
+    fontSize: 30,
+    fontWeight: "600",
+    color: "#000000",
+    textTransform: "capitalize",
+  },
+  itemNameWrapper: {},
+  itemName: {
+    fontSize: 30,
+    fontWeight: "600",
+    color: "#000000",
+    textTransform: "capitalize",
+  },
+  itemPriceWrapper: {},
+  itemPrice: {
+    fontSize: 30,
+    fontWeight: "600",
+    color: "#F47A0A",
+    textTransform: "capitalize",
+  },
+});
